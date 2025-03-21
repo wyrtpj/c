ea74c1a2664dd8bf43236fffcc84c151c6cc1829
маш маш маш <mtnkuv@mail.ru>
2025-03-21 11:06:45 +0300

    Update and rename задача 3 to задача 3.c

--- задача 3.c
+++ задача 3.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
+
 int main() {
-    int x,y,z;
-   printf("введите три числа : \n");
+int x,y,z;
+    
+printf("введите три числа : \n");
 scanf("%d %d %d",&x, &y,&z);
-int result= x*y*z;
+    
+int result = x*y*z;
 int sign= (result>0)-(result<0);
+    
 printf("%d\n",sign);
+
+    
     return 0;
 }
