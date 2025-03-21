fe8f7785fa62a61a8c59e4362dbda6dbe2534146
маш маш маш <mtnkuv@mail.ru>
2025-03-21 11:07:26 +0300

    Update and rename задача 4 to задача 4.c

--- задача 4.c
+++ задача 4.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
+
 int main() {
-    int x;
-   printf("введите числo : \n");
+int x;
+    
+printf("введите числo : \n");
 scanf("%d",&x);
+    
 printf("%d\n",(x>0) && (x&(x-1))==0);
+    
     return 0;
 }
