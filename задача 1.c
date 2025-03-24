4390040438b5c9cee7249b5320b2d599f589b970
маш маш маш <mtnkuv@mail.ru>
2025-03-24 12:33:57 +0300

    Update задача 1.c

--- задача 1.c
+++ задача 1.c
@@ -3,15 +3,15 @@
 int main() { 
    float temperature;
 
-   printf( "введите температуру в Цельсия: \n" );
-   scanf( "%f", &temperature); 
-   printf( "c=%.2f; ", temperature); 
+   printf("введите температуру в Цельсия: \n");
+   scanf("%f", &temperature); 
+   printf("c=%.2f; ", temperature); 
    
    float farengate;
    
-   farengate = ( temperature*9.0/5.0)  + 32; 
+   farengate = (temperature * 9.0 / 5.0) + 32; 
    
-   printf( "F=%.2f", farengate, "\n"); 
+   printf("F=%.2f", farengate, "\n"); 
    
    return 0; 
 }
