f231bd98e2680cfa06973b677b858edc65366365
маш маш маш <mtnkuv@mail.ru>
2025-03-24 12:35:48 +0300

    Update задача 2.c

--- задача 2.c
+++ задача 2.c
@@ -4,16 +4,16 @@
 int main() { 
     int num; 
     
-    printf( "введите целое число: \n" ); 
-    scanf( "%d",&num ); 
-    printf( "Число = %d; ",num ); 
+    printf("введите целое число: \n"); 
+    scanf("%d",&num); 
+    printf("Число = %d; ",num); 
     
     num= num % 2; 
     bool chetni = (num==0); 
     
-    const char*resultat=chetni ? "число четное" : " число не четное"; 
+    const char * resultat=chetni ? "число четное" : " число не четное"; 
     
-    printf("%s\n",resultat ); 
+    printf("%s\n",resultat); 
     
     return 0;
 }
