bb1d115f872797c3faf57b611292db44b10b5069
маш маш маш <mtnkuv@mail.ru>
2025-03-24 12:37:36 +0300

    Update задача 4.c

--- задача 4.c
+++ задача 4.c
@@ -6,7 +6,7 @@ int main() {
     printf("введите числo : \n"); 
     scanf("%d",&x); 
     
-    printf("%d\n",(x>0) && (x&(x-1))==0); 
+    printf("%d\n", (x > 0) && (x & (x - 1)) == 0); 
     
     return 0;
 }
